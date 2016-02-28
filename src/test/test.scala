a97f1f0eb193f106eced7a7ba33d6d56bb582ad5
fqc <337940626@qq.com>
2016-02-28 15:09:41 +0800

    for with if (filter style)

--- src/test/test.scala
+++ src/test/test.scala
@@ -6,32 +6,42 @@ package test
  */
 object test {
 
-  def add(a: Int, b: Int):Int = {
+  def add(a: Int, b: Int): Int = {
     a + b
   }
 
   def main(args: Array[String]) {
-//    println(add(3,5))
-      printDirFiles()
+    //    println(add(3,5))
+    //printDirFiles()
+    printDirFilesWithFilter()
   }
 
 
   /**
    * filesHere
    */
-  def printDirFiles(): Unit ={
+  def printDirFiles(): Unit = {
     var filesHere = (new java.io.File(".")).listFiles()
-    for (file <- filesHere ) {
+    for (file <- filesHere) {
       println(file)
     }
     //间接访问（不推荐）
-    for (file <- 0 to filesHere.length-1) {
-       println(filesHere(file))
+    for (file <- 0 to filesHere.length - 1) {
+      println(filesHere(file))
     }
 
+    for (i <- 0 to 5) { print(i) }
+    for (i <- 0 until 5) { print(i) }
   }
 
+  def printDirFilesWithFilter(): Unit = {
+    var filesHere = (new java.io.File(".")).listFiles()
+    for (file <- filesHere
+         if file.getName.endsWith(".md")
+         if file.isFile)
+    println(file) //for 省略时 默认后一句是在{}中
+    println(1 to 11)
 
-
+  }
 
 }
